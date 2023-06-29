cf5dfd0e22cf60ea008ce651d87314699319aa43
unseen <122075091+maliciously@users.noreply.github.com>
2023-06-29 09:20:03 -0400

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,9 +1,8 @@
 function copyDiscord() {
-    console.log("test");
-    navigator.clipboard.writeText("unseen#0001");
+    navigator.clipboard.writeText("unvisited");
     document.getElementsByClassName("discord")[0].innerHTML = "Tag Copied!";
     setTimeout(() => {
-        document.getElementsByClassName("discord")[0].innerHTML = "unseen#0001";
+        document.getElementsByClassName("discord")[0].innerHTML = "unvisited";
 
     }, 1250);
  }
