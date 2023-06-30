5e88fa1f1c8a60c39d3e74ed652e85db145310bc
maliciously <unseen@dox.rip>
2023-06-30 07:28:30 -0400

    Major Revamp

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,8 +1,9 @@
-function copyDiscord() {
-    navigator.clipboard.writeText("unvisited");
-    document.getElementsByClassName("discord")[0].innerHTML = "Tag Copied!";
-    setTimeout(() => {
-        document.getElementsByClassName("discord")[0].innerHTML = "unvisited";
+// no longer used with current version
+// function copyDiscord() {
+//     navigator.clipboard.writeText("unvisited");
+//     document.getElementsByClassName("discord")[0].innerHTML = "Tag Copied!";
+//     setTimeout(() => {
+//         document.getElementsByClassName("discord")[0].innerHTML = "unvisited";
 
-    }, 1250);
- }
+//     }, 1250);
+//  }
